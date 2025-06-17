63a3a948de32e5909a1e60d69ce917f41770e8e7
Maria <mimozka04@gmail.com>
2025-06-17 09:59:46 +0300

    register CustomUser

--- fermamarket_02/customusers/admin.py
+++ fermamarket_02/customusers/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('username', 'email', 'is_staff', 'is_superuser')
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+
+    fieldsets = UserAdmin.fieldsets  # Използваме оригиналните без дублиране
